c650022e8d0950b7739e3ba730b89c84d3f5f229
RafaelSouza115 <Rafael.asantos@Sptech.school>
2025-11-18 00:14:16 -0300

    Script + modelagem logica do Banco de dados feita. tela home, sobre, cadastro e login front feito. tela de login e cadastro com backend feito, fazendo conexão ao banco de dados.

--- Aplication/src/database/script-tabelas.sql
+++ Aplication/src/database/script-tabelas.sql
@@ -1,64 +1,38 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql server
-*/
-
 CREATE DATABASE rebobinando;
-
 USE rebobinando;
 
-CREATE TABLE empresa (
+CREATE TABLE preferencia(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+	cantor VARCHAR(50)
 );
+INSERT INTO preferencia(cantor) VALUES 
+('Arnaldo Antunes'),
+('Carlinhos Brown'),
+('Marisa Monte');
 
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
 	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
+	id_preferencia int,
+	constraint fk_preferencia FOREIGN KEY (id_preferencia) REFERENCES preferencia(id)
+	);
 
-CREATE TABLE aviso (
+create table quiz (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+	nome VARCHAR(100)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
+CREATE TABLE jogada(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+	id_usuario INT,
+	id_quiz INT,
+	acertos INT,
+	erros INT,
+	constraint fk_usuario FOREIGN KEY (id_usuario) REFERENCES usuario (id),
+	constraint fk_quiz FOREIGN KEY (id_usuario) REFERENCES quiz (id)
 );
+SELECT * from usuario;
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
-
-REFERENCES aquario(id);
+SELECT * FROM preferencia;
